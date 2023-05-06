96dd2fb4efa3834b07b5e61cd69d1396a97576a2
Gerarvvdor <00053520@uca.edu.sv>
2023-05-05 21:32:23 -0600

    labo07 el bueno

---
+++ Labo07-2/app/src/main/java/com/example/laboratorio05/ui/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.laboratorio05.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.example.laboratorio05.R
+import com.example.laboratorio05.databinding.ActivityMainBinding
+import com.example.laboratorio05.databinding.FragmentNewMovieBinding
+import com.example.laboratorio05.ui.movie.MovieViewModel
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MovieViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
